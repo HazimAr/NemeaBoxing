3bcdbaa3d2fffdae05ab9f4c372aaabd03c61c16
Hazim Arafa <hazimarafa80@gmail.com>
2024-02-29 11:36:40 -0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 const ANDROID_APP_URL = "";
 const IOS_APP_URL = "";
 
-const MIXPANEL_TOKEN = "";
+const MIXPANEL_TOKEN = "eb8958a53c08accd9dc990f22272401d";
 
 const IS_PRODUCTION = process.env.NODE_ENV === "production";
 
