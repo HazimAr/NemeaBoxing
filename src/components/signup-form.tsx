d8afa49914c68e6a7d0f18d1317ed5d8a06ebe05
Hazim Arafa <hazimarafa80@gmail.com>
2024-03-18 17:18:46 -0700

    Update signup-form.tsx

--- src/components/signup-form.tsx
+++ src/components/signup-form.tsx
@@ -77,7 +77,7 @@ export default function SignUpForm({ noPrograms = false }) {
 				src="https://link.localbestgyms.com/widget/form/ThOJk3ISw2RyNker03X1"
 				style={{
 					width: "100%",
-					height: "800px",
+					height: "700px",
 					border: "none",
 					borderRadius: "4px",
 				}}
