578c85b261712ae82bf4337cd8405f12c877c254
Hazim Arafa <hazimarafa80@gmail.com>
2024-04-01 21:19:37 -0700

    Update route.ts

--- src/app/api/send-text/route.ts
+++ src/app/api/send-text/route.ts
@@ -2,10 +2,11 @@ import { sendText } from "@lib/sendText";
 
 export async function POST(request: Request) {
 	const body = await request.json();
-	const response = await sendText({
-		to: "7029291601",
-		body,
-	});
+	console.log(body);
+	// const response = await sendText({
+	// 	to: "7029291601",
+	// 	body,
+	// });
 
 	if (!response.errorCode)
 		return new Response(response.errorMessage, { status: 500 });
