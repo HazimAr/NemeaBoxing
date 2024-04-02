cf39d0242642ebd2224546b5ea5bc109b7b413a6
Hazim Arafa <hazimarafa80@gmail.com>
2024-04-01 21:13:43 -0700

    Update route.ts

--- src/app/api/send-text/route.ts
+++ src/app/api/send-text/route.ts
@@ -7,9 +7,8 @@ export async function POST(request: Request) {
 		body,
 	});
 
-	if (!response.errorCode) {
+	if (!response.errorCode)
 		return new Response(response.errorMessage, { status: 500 });
-	}
 
 	return new Response(null, { status: 200 });
 }
