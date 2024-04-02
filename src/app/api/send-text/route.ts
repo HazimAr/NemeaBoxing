a495cd7321a59944227952f0a9a7bfd18e6eb764
Hazim Arafa <hazimarafa80@gmail.com>
2024-04-01 21:33:51 -0700

    Update route.ts

--- src/app/api/send-text/route.ts
+++ src/app/api/send-text/route.ts
@@ -1,15 +1,14 @@
-// import { sendText } from "@lib/sendText";
+import { sendText } from "@lib/sendText";
 
 export async function POST(request: Request) {
 	const body = await request.json();
-	console.log(body);
-	// const response = await sendText({
-	// 	to: "7029291601",
-	// 	body,
-	// });
+	const response = await sendText({
+		to: "7029291601",
+		body: `New lead from website!\nName: ${body.full_name}\nEmail: ${body.email}\nPhone: ${body.phone}`,
+	});
 
-	// if (!response.errorCode)
-	// 	return new Response(response.errorMessage, { status: 500 });
+	if (!response.errorCode)
+		return new Response(response.errorMessage, { status: 500 });
 
 	return new Response(null, { status: 200 });
 }
